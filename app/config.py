4dd8df5dba6bb113471dabd0ea4ba197d24271bf
Xerz <alehag@ya.ru>
2026-03-13 15:00:21 +0500

    Лишнее

--- app/config.py
+++ app/config.py
@@ -1,4 +1,3 @@
 DB_PATH = "stations20260313.db"
-STATIONS_URL = "https://services.drova.io/server-manager/servers/public/web"
 PRODUCTS_URL = "https://services.drova.io/product-manager/product/listfull2"
 CACHE_TTL_SECONDS = 600
